e18f36ad31485c518f6757bfdd652ba5a2fd26d7
dpinkus87 <dylanpinkus@gmail.com>
2023-01-05 15:20:06 -0600

    Updated routes and index relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,18 +5,23 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "",
-  onDelete: "",
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 })
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "",
-  onDelete: "",
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsTo(Tag, {})  
+Product.belongsToMany(Tag, {
+  through: "ProductTag",
+})  
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: "ProductTag",
+})
 
 module.exports = {Product, Category, Tag, ProductTag};
