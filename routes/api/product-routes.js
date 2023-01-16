dd27ec7488eb432d38096754815123ed9e6a62a9
dpinkus87 <dylanpinkus@gmail.com>
2023-01-16 09:54:19 -0600

    Updated delete route, added comma

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -115,7 +115,7 @@ try {
   const productData = await Product.destroy({
     where: {
       id: req.params.id
-    }
+    },
   });
   if(!productData) {
     res.status(404).json({ message: "No product found with this id!"})
